eb6c4912725731d75816c90bd8bb37a1e119fb14
mkp246 <mkp246@gmail.com>
2017-08-21 00:43:15 +0530

    basic gradle project

---
+++ src/main/java/net/manoj/gradleTutorial/Main.java
@@ -0,0 +1,9 @@
+package net.manoj.gradleTutorial;
+
+import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
+public class Main {
+  public static void main(String[] args){
+    System.out.println(StringUtils.swapCase(Arrays.toString(args)));
+  }
+}
